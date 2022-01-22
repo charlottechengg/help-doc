f09da10e9cbaabde73cfcde3f2454f3b59a17dff
Senni Tan <sennitan@geotab.com>
2022-01-22 12:01:52 -0500

    set up assembly ai server and button

--- frontend/src/components/Dashboard.js
+++ frontend/src/components/Dashboard.js
@@ -1,18 +1,23 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { Button, Grid, IconButton } from '@mui/material';
 import Dropdown from './Dropdown';
+
+
 const Dashboard = ()  => {
+    const [isRecording, setRecording] = useState(false)
+    const [text, setText] = useState('')
+
     return (
         <Grid justify="space-between" alignItems="center" container>
             <Grid item>
                 <Dropdown/>
             </Grid>
             <Grid item>
-                        <Button>Add</Button>
+                <Button onClick={() => {setRecording(!isRecording)}}>{isRecording? 'Stop' : 'Start'}</Button>
             </Grid>
             <Grid item>
-                        <Button>Translate</Button>
-                    </Grid>
+                <Button>Translate</Button>
+            </Grid>
         </Grid>
     );
 };
