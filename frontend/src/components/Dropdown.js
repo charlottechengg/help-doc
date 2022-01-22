e84d5884805a0decf5fcf2e0397174aef7de0bb4
Manyi Cheng <chengm33@mcmaster.ca>
2022-01-22 10:48:00 -0500

    added pages

--- frontend/src/components/Dropdown.js
+++ frontend/src/components/Dropdown.js
@@ -5,33 +5,33 @@ import Autocomplete from '@mui/material/Autocomplete';
 export default function Dropdown() {
 	return (
 		<Autocomplete
-		id="dropdown-list"
-		sx={{ width: 300 }}
-		options={Languages}
-		autoHighlight
-		getOptionLabel={(option) => option.label}
-		renderOption={(props, option) => (
-			<Box component="li" sx={{ '& > img': { mr: 2, flexShrink: 0 } }} {...props}>
-			{/* <img
-				loading="lazy"
-				width="20"
-				src={`https://flagcdn.com/w20/${option.code.toLowerCase()}.png`}
-				srcSet={`https://flagcdn.com/w40/${option.code.toLowerCase()}.png 2x`}
-				alt=""
-			/> */}
-			{option.label} ({option.code})
-			</Box>
-		)}
-		renderInput={(params) => (
-			<TextField
-			{...params}
-			label="Choose a language"
-			inputProps={{
-				...params.inputProps,
-				autoComplete: 'new-password', // disable autocomplete and autofill
-			}}
-			/>
-		)}
+			id="dropdown-list"
+			sx={{ width: 300 }}
+			options={Languages}
+			autoHighlight
+			getOptionLabel={(option) => option.label}
+			renderOption={(props, option) => (
+				<Box component="li" sx={{ '& > img': { mr: 2, flexShrink: 0 } }} {...props}>
+				{/* <img
+					loading="lazy"
+					width="20"
+					src={`https://flagcdn.com/w20/${option.code.toLowerCase()}.png`}
+					srcSet={`https://flagcdn.com/w40/${option.code.toLowerCase()}.png 2x`}
+					alt=""
+				/> */}
+				{option.label} ({option.code})
+				</Box>
+			)}
+			renderInput={(params) => (
+				<TextField
+				{...params}
+				label="Choose a language"
+				inputProps={{
+					...params.inputProps,
+					autoComplete: 'new-password', // disable autocomplete and autofill
+				}}
+				/>
+			)}
 		/>
 	);
 }
