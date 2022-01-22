d5d5b59028e40956d22957c78c5ec43dd8bd7da6
Senni Tan <sennitan@geotab.com>
2022-01-22 12:12:24 -0500

    add text box

--- frontend/src/components/Dashboard.js
+++ frontend/src/components/Dashboard.js
@@ -1,24 +1,36 @@
 import React, {useState} from 'react';
-import { Button, Grid, IconButton } from '@mui/material';
+import { Button, Grid, IconButton, Box } from '@mui/material';
 import Dropdown from './Dropdown';
 
+
+const commonStyles = {
+    bgcolor: 'background.paper',
+    m: 1,
+    borderColor: 'text.primary',
+    width: '50rem',
+    height: '50rem',
+  };
 
 const Dashboard = ()  => {
     const [isRecording, setRecording] = useState(false)
     const [text, setText] = useState('')
 
     return (
-        <Grid justify="space-between" alignItems="center" container>
-            <Grid item>
-                <Dropdown/>
+        <>
+            <Grid justify="space-between" alignItems="center" container>
+                <Grid item>
+                    <Dropdown/>
+                </Grid>
+                <Grid item>
+                    <Button onClick={() => {setRecording(!isRecording)}}>{isRecording? 'Stop' : 'Start'}</Button>
+                </Grid>
+                <Grid item>
+                    <Button>Translate</Button>
+                </Grid>
             </Grid>
-            <Grid item>
-                <Button onClick={() => {setRecording(!isRecording)}}>{isRecording? 'Stop' : 'Start'}</Button>
-            </Grid>
-            <Grid item>
-                <Button>Translate</Button>
-            </Grid>
-        </Grid>
+
+            <Box sx={{ ...commonStyles, border: 1 }}>Something here</Box>
+        </>
     );
 };
 
